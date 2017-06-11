9529aa414315bc5b88ac5866e34437d4c60ccf1c
lmcnally <luke.mcnally@cosworth.com>
2017-06-11 10:33:53 +0100

    Added skeleton Android app.

---
+++ src/LMcNally.Xamarin.MvpDemo.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LMcNally.Xamarin.MvpDemo.Droid
+{
+	[Activity(Label = "LMcNally.Xamarin.MvpDemo.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			// SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
